2a19c565999257eba039669aa02601a97bdd9cc9
trekpio2 <piotrmichalak2000@o2.pl>
2023-10-24 19:45:20 +0200

    entities, weather controller

---
+++ src/Controller/WeatherController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Location;
+use App\Repository\ForecastRepository;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WeatherController extends AbstractController
+{
+    #[Route('/weather/{city}/{country_code}', name: 'app_weather')]
+    public function city(
+        #[MapEntity(mapping: ['city' => 'city', 'country_code' => 'country_code'])]
+        Location $location,
+        ForecastRepository $repository,
+        string $country_code = 'PL'): Response
+    {
+        $measurements = $repository->findByLocation($location);
+
+        return $this->render('weather/city.html.twig', [
+            'location' => $location,
+            'measurements' => $measurements,
+        ]);
+
+    }
+}
---
+++ src/Entity/Forecast.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ForecastRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ForecastRepository::class)]
+class Forecast
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: '0')]
+    private ?string $temperature = null;
+
+    #[ORM\ManyToOne(inversedBy: 'forecasts')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Location $location = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 3, scale: '0')]
+    private ?string $wind = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTemperature(): ?string
+    {
+        return $this->temperature;
+    }
+
+    public function setTemperature(string $temperature): static
+    {
+        $this->temperature = $temperature;
+
+        return $this;
+    }
+
+    public function getLocation(): ?Location
+    {
+        return $this->location;
+    }
+
+    public function setLocation(?Location $location): static
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getWind(): ?string
+    {
+        return $this->wind;
+    }
+
+    public function setWind(string $wind): static
+    {
+        $this->wind = $wind;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
