6548df0c6146768cd71872dc5f1f1842d369fc2a
trekpio2 <piotrmichalak2000@o2.pl>
2023-11-05 10:51:52 +0100

    security - login, logout, roles with hierarchy

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -8,10 +8,12 @@ use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class WeatherController extends AbstractController
 {
     #[Route('/weather/{city}/{country_code}', name: 'app_weather')]
+    #[IsGranted('ROLE_WEATHER_CITY')]
     public function city(
         #[MapEntity(mapping: ['city' => 'city', 'country_code' => 'country_code'])]
         Location $location,
